0dad2f1e4ff22953f269d8e1916741a9b84c0bc6
karl <karlskibba@t-online.de>
2022-02-15 15:11:45 +0100

    Documentation addedl.

--- Work/report.py
+++ Work/report.py
@@ -16,34 +16,18 @@ def read_portfolio(filename: str) -> List[dict]:
                 portfolio.append(holding)                
             except ValueError:
                 print('Bad row:', row)
-
     return portfolio
 
 def read_prices(filename: str) -> dict:
     prices = {}
-#    line_num = 0
     with open(filename) as f:
         rows = csv.reader(f)
         for line_num, row in enumerate(rows, start=1):
-#            line_num += 1
             try:
                 prices[row[0]] = float(row[1])
             except IndexError:
                 print(f'Bad row at line {line_num}:', row)
     return prices
-
-def portfolio_report(portfolio_fn: str, prices_fn: str) -> List[list]:
-    '''
-    Generates a portfolio report from a portfolio- and prices-file
-    '''
-    portfolio = read_portfolio(portfolio_fn)
-    prices    = read_prices(prices_fn)
-    report = [(h['name'], 
-               h['shares'], 
-               h['price'], 
-               round(prices[h['name']]-h['price'], 2)) 
-            for h in portfolio]
-    return report
 
 
 def print_report(report: Iterable[list]) -> None:
@@ -58,5 +42,29 @@ def print_report(report: Iterable[list]) -> None:
         print('%10s %10s %10s %10s'  % row)
 
 
+def portfolio_report(portfolio_fn: str, prices_fn: str) -> None:
+    '''
+    Generates a portfolio report from a portfolio- and prices-file and print it 
+    out as a table e.g. 
+              Name     Shares      Price     Change
+        ---------- ---------- ---------- ---------- 
+                AA        100       32.2     -22.98
+               IBM         50       91.1      15.18
+               CAT        150      83.44     -47.98
+    '''
+    portfolio = read_portfolio(portfolio_fn)
+    prices    = read_prices(prices_fn)
+    report = [(h['name'], 
+               h['shares'], 
+               h['price'], 
+               round(prices[h['name']]-h['price'], 2)) 
+            for h in portfolio]
+    print_report(report)
 
 
+
+if __name__ == "__main__":
+    portfolio_fn = 'Data/portfolio.csv'
+    prices_fn    = 'Data/prices.csv'
+
+    portfolio_report(portfolio_fn, prices_fn)
