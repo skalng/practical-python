7a649c7e21fbcd3b77a47cc3962050b7cacfcc4a
karl <karlskibba@t-online.de>
2022-02-15 14:16:09 +0100

    bounce.py programmed.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+bounces = 0
+
+
+while bounces < 10:
+    bounces += 1
+    height = height - 0.4 * height
+    print(bounces, end = ' ')
+    print(round(height, 3))
+
