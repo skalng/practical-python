87da7d3edd984c53120bc259f773de2485c92eb4
karl <karlskibba@t-online.de>
2022-02-17 12:09:05 +0100

    stock.py created.

---
+++ Work/stock.py
@@ -0,0 +1,41 @@
+'''
+Created on 16.02.2022
+
+@author: ho_ksk
+__updated__='2022-02-17 12:05:34'
+
+
+Exercise 4.1/.2
+'''
+
+
+class Stock(object):
+    
+    def __init__(self, name, shares, price):
+        self.name   = name
+        self.shares = shares
+        self.price  = price
+        
+        
+    def cost(self) -> float:
+        return self.shares * self.price
+        
+        
+    def sell(self, number: int) -> None:
+        self.shares = self.shares - number
+        
+        
+        
+if __name__ == '__main__':
+    import fileparse
+    
+    with open('Data/portfolio.csv') as lines:
+        portdicts = fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
+        
+    portfolio = [Stock(d['name'], d['shares'], d['price']) for d in portdicts]
+    print(sum([s.cost() for s in portfolio]))
+    
+
+    
+    
+        
