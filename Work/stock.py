021922e934a1afdf6cf4576b3fb7e279bb401933
karl <karlskibba@t-online.de>
2022-02-23 16:10:32 +0100

    Properties introduced.

--- Work/stock.py
+++ Work/stock.py
@@ -2,14 +2,16 @@
 Created on 16.02.2022
 
 @author: ho_ksk
-__updated__='2022-02-18 14:21:25'
+__updated__='2022-02-23 15:53:55'
 
 
 Exercise 4.1/.2
 '''
+from os import name
 
 
 class Stock(object):
+    __slotts__ = ('name', 'shares', 'price')
     
     def __init__(self, name, shares, price):
         self.name   = name
@@ -18,8 +20,45 @@ class Stock(object):
         
     def __repr__(self):
         return f'Stock({self.name}, {self.shares}, {self.price})'
-        
-        
+    
+    ''' --- properies --- '''
+    @property
+    def name(self):
+        return self._name
+     
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str):
+            self._name = value
+        else:
+            raise TypeError('str expected')
+    
+    @property
+    def shares(self):
+        return self._shares
+    
+    @shares.setter
+    def shares(self, value):
+        if isinstance(value, int):
+            self._shares = value
+        else:
+            raise TypeError('int expected')
+    
+ 
+    @property
+    def price(self):
+        return self._price
+     
+    @price.setter
+    def price(self, value):
+        if isinstance(value, float):
+            self._price = value
+        else:
+            raise TypeError('float expected')
+    
+ 
+    ''' --- methods ---'''
+    @property    
     def cost(self) -> float:
         return self.shares * self.price
         
@@ -36,7 +75,7 @@ if __name__ == '__main__':
         portdicts = fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
         
     portfolio = [Stock(d['name'], d['shares'], d['price']) for d in portdicts]
-    print(sum([s.cost() for s in portfolio]), 'expected is: 44671.15')
+    print(sum([s.cost for s in portfolio]), 'expected is: 44671.15')
     
 
     
