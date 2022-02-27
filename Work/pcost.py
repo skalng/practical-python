9e1d379bae34dc7e7840f30d0101cc39c83749f8
karl <karlskibba@t-online.de>
2022-02-27 16:50:49 +0100

    Reworked based on new class Portfolio.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,28 +1,17 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.27 / 6.2 
  
-import sys
-import csv
+
+from report import read_portfolio
 
 def portfolio_cost(filename):
-    total_cost = 0.0
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for rowno, row in enumerate(rows):
-            try:
-                h = dict(zip(headers, row))
-                total_cost += int(h['shares']) * float(h['price'])
-            except ValueError:
-                print(f'Bad row at {rowno}:', row)
 
-    return total_cost
+    portfolio = read_portfolio(filename)
+    return portfolio.total_cost
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = 'Data/portfolio.csv'
 
-cost = portfolio_cost(filename)
-print('Total cost:', cost)
+
+if __name__ == '__main__':
+    
+    print(portfolio_cost('Data/portfolio.csv'))
