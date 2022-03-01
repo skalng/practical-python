fb578e644c28500b0ec6bef4485ffcf1359ac3ef
karl <karlskibba@t-online.de>
2022-03-01 22:40:22 +0100

    Exercise 6.5 done.

---
+++ Work/follow.py
@@ -0,0 +1,30 @@
+'''
+Created on 01.03.2022
+
+@author: ho_ksk
+__updated__='2022-03-01 20:13:33'
+'''
+
+
+# follow.py
+
+import os
+import time
+
+f = open('Data/stocklog.csv')
+f.seek(0, os.SEEK_END)
+
+while True:
+    line = f.readline()
+    if line == '':
+        time.sleep(0.1)
+        continue
+    fields = line.split(',')
+    name   = fields[0].strip('"')
+    price  = float(fields[1])
+    change = float(fields[4])
+    if change < 0:
+        print(f'{name:>10s}{price:>10f}{change:>10f}')
+
+
+
