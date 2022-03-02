9f2aabf5a5a309139d87fd0be596e9848ec6bf26
karl <karlskibba@t-online.de>
2022-03-02 10:56:25 +0100

    Exercise 6.8 finished.

--- Work/follow.py
+++ Work/follow.py
@@ -2,7 +2,7 @@
 Created on 01.03.2022
 
 @author: ho_ksk
-__updated__='2022-03-01 20:13:33'
+__updated__='2022-03-02 10:54:29'
 '''
 
 
@@ -10,21 +10,63 @@ __updated__='2022-03-01 20:13:33'
 
 import os
 import time
+from report import read_portfolio
+from mailcap import subst
 
-f = open('Data/stocklog.csv')
-f.seek(0, os.SEEK_END)
+# f = open('Data/stocklog.csv')
+# f.seek(0, os.SEEK_END)
+# 
+# while True:
+#     line = f.readline()
+#     if line == '':
+#         time.sleep(0.1)
+#         continue
+#     fields = line.split(',')
+#     name   = fields[0].strip('"')
+#     price  = float(fields[1])
+#     change = float(fields[4])
+#     if change < 0:
+#         print(f'{name:>10s}{price:>10f}{change:>10f}')
 
-while True:
-    line = f.readline()
-    if line == '':
-        time.sleep(0.1)
-        continue
-    fields = line.split(',')
-    name   = fields[0].strip('"')
-    price  = float(fields[1])
-    change = float(fields[4])
-    if change < 0:
-        print(f'{name:>10s}{price:>10f}{change:>10f}')
+def follow(filename):
+    f = open(filename)
+    f.seek(0, os.SEEK_END)
+    while True:
+        line = f.readline()
+        if line == '':
+            time.sleep(0.1)
+            continue
+        yield line   
+
+
+def fielematch(lines, substr):
+    for line in lines:
+        if substr in line:
+            yield line
 
 
 
+if __name__ == '__main__':
+#     import report
+#     
+#     portfolio = report.read_portfolio('Data/portfolio.csv')
+#     
+#     
+#     for line in follow('Data/stocklog.csv'):
+#         fields = line.split(',')
+#         name   = fields[0].strip('"')
+#         price  = float(fields[1])
+#         change = float(fields[4])
+#         if name in portfolio:
+#             print(f'{name:>10s}{price:>10.2f}{change:>10.2f}')
+    
+    lines = follow('Data/stocklog.csv')
+    ibm = fielematch(lines, 'IBM')
+    for line in ibm:
+        print(line)
+    
+    
+    
+    
+    
+    
