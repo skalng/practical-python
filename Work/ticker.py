ac33c30e2d04e82f17b89cef4531022e983b1105
karl <karlskibba@t-online.de>
2022-03-02 23:59:44 +0100

    Exercise 6.12 finished by creating function ticker.

--- Work/ticker.py
+++ Work/ticker.py
@@ -2,15 +2,22 @@
 Created on 02.03.2022
 
 @author: ho_ksk
-__updated__='2022-03-02 14:45:00'
+__updated__='2022-03-02 23:57:28'
 '''
 
-# ticker.py - Exercise 6.10
+# ticker.py - Exercise 6.10 / 6.11 / 6.12
 
-
-from follow import follow
 import csv
-from _testbuffer import slice_indices
+
+import report
+from follow import follow
+
+portfolio = report.read_portfolio('Data/portfolio.csv')
+
+def filter_symbols(rows, names):
+    for row in rows:
+        if row['name'] in names:
+            yield row
 
 
 def make_dicts(rows, headers):
@@ -33,17 +40,40 @@ def parse_stock_data(lines):
     rows = select_coloums(rows, [0, 1, 4])
     rows = convert_types(rows, [str, float, float])
     rows = make_dicts(rows, ['name', 'price', 'change'])
+    rows = filter_symbols(rows, portfolio)
     return rows
+
+
+def ticker(portfile: str, logfile: str, fmt: str):
+    
+    portfolio = report.read_portfolio(portfile)
+    lines = follow(logfile)
+    rows = csv.reader(lines)
+    rows = select_coloums(rows, [0, 1, 4])
+    rows = convert_types(rows, [str, float, float])
+    rows = make_dicts(rows, ['name', 'price', 'change'])
+    rows = filter_symbols(rows, portfolio)
+    
+    from tableformat import create_formatter
+    
+    formatter = create_formatter(fmt)
+    formatter.headings(['Name', 'Price', 'Change'])
+    for row in rows:
+        formatter.row([ v for _, v in row.items()])
+    
+   
+    
 
 
 
 if __name__ == '__main__':
-    lines = follow('Data/stocklog.csv')
-    rows  = parse_stock_data(lines)
-    for row in rows:
-        print(row)
+#     lines = follow('Data/stocklog.csv')
+#     rows  = parse_stock_data(lines)
+#     for row in rows:
+#         print(row)
 
-
+    ticker('Data/portfolio.csv', 'Data/stocklog.csv', 'csv')
+    
 
 
 
