dcd435b8a1786bf80156f70f8649449124ba1d4d
karl <karlskibba@t-online.de>
2022-03-03 10:26:20 +0100

    Chapter 6 finished and reworked based on Solutions 6_15

--- Work/ticker.py
+++ Work/ticker.py
@@ -2,7 +2,7 @@
 Created on 02.03.2022
 
 @author: ho_ksk
-__updated__='2022-03-02 23:57:28'
+__updated__='2022-03-03 10:24:41'
 '''
 
 # ticker.py - Exercise 6.10 / 6.11 / 6.12
@@ -10,19 +10,19 @@ __updated__='2022-03-02 23:57:28'
 import csv
 
 import report
+import tableformat
 from follow import follow
 
 portfolio = report.read_portfolio('Data/portfolio.csv')
 
 def filter_symbols(rows, names):
-    for row in rows:
-        if row['name'] in names:
-            yield row
+    return (row for row in rows if row['name'] in names)
 
 
 def make_dicts(rows, headers):
-    for row in rows:
-        yield {h: val for h, val in zip(headers, row)}
+    return (dict(zip(headers, row)) for row in rows)
+#     for row in rows:
+#         yield dict(zip(headers, row))
 
 
 def convert_types(rows, types):
@@ -40,7 +40,6 @@ def parse_stock_data(lines):
     rows = select_coloums(rows, [0, 1, 4])
     rows = convert_types(rows, [str, float, float])
     rows = make_dicts(rows, ['name', 'price', 'change'])
-    rows = filter_symbols(rows, portfolio)
     return rows
 
 
@@ -48,18 +47,13 @@ def ticker(portfile: str, logfile: str, fmt: str):
     
     portfolio = report.read_portfolio(portfile)
     lines = follow(logfile)
-    rows = csv.reader(lines)
-    rows = select_coloums(rows, [0, 1, 4])
-    rows = convert_types(rows, [str, float, float])
-    rows = make_dicts(rows, ['name', 'price', 'change'])
-    rows = filter_symbols(rows, portfolio)
-    
-    from tableformat import create_formatter
-    
-    formatter = create_formatter(fmt)
+    rows = parse_stock_data(lines)
+    rows = (row for row in rows if row['name'] in portfolio)
+        
+    formatter = tableformat.create_formatter(fmt)
     formatter.headings(['Name', 'Price', 'Change'])
     for row in rows:
-        formatter.row([ v for _, v in row.items()])
+        formatter.row([ row['name'], row['price'], row['change']])
     
    
     
