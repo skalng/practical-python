90060514f4b150d7eb055720e89518f380bc9422
karl <karlskibba@t-online.de>
2022-03-06 17:04:29 +0100

    Reworked based on the solution.

--- Work/timethis.py
+++ Work/timethis.py
@@ -2,19 +2,31 @@
 Created on 06.03.2022
 
 @author: ho_ksk
-__updated__='2022-03-06 16:11:09'
+__updated__='2022-03-06 16:23:44'
 '''
 
 # timethis.py
 
+# Execise 7.10
+
+import time
+
 def timethis(func):
     def wrapper(*args, **kwargs):
-        import time
         t1 = time.time()
-        func(*args, **kwargs)
-        print(func.__name__ + 'runs' +  time.time()-t1)
-        return
+        try:
+            return func(*args, **kwargs)
+        finally:
+            t2 = time.time()
+            print(f'{func.__module__}.{func.__name__} runs {t1-t2}')
     return wrapper
 
 
+if __name__ == '__main__':
+    @timethis
+    def countdown(n):
+        while n > 0:
+            n-= 1
+
+    countdown(1000000)
 
