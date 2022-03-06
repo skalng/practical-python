4613f06fd2ef17a7a31a40b75e4d063ec455cd4e
karl <karlskibba@t-online.de>
2022-03-07 00:22:51 +0100

    Exercise 7.11 finished with help by solutions.

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -2,49 +2,70 @@
 Created on 26.02.2022
 
 @author: ho_ksk
-__updated__='2022-02-27 17:04:21'
+__updated__='2022-03-07 00:20:45'
 '''
 
 # portfolio.py
 
-from typing import List
-from stock import Stock
+
+# Exercise 7:11
+
+import fileparse
+import stock
 
 class Portfolio:
     
-    def __init__(self, holdings: List[Stock]):
-        self._hloldings = holdings
+    def __init__(self):
+        self._holdings = []
+        
+    @classmethod        
+    def from_csv(cls, lines, **opts):
+        self = cls()
+        portfoliodict = fileparse.parse_csv(lines, 
+                                            select=['name', 'shares', 'price'], 
+                                            types=[str, int, float],
+                                            **opts)
+        for d in portfoliodict:
+            self.append(stock.Stock(**d))
+            
+        return self
+
+    def append(self, holding):
+        self._holdings.append(holding)
+
+        
         
     def __iter__(self):
-        return self._hloldings.__iter__()
+        return self._holdings.__iter__()
     
     def __len__(self):
-        return len(self._hloldings)
+        return len(self._holdings)
     
     def __getitem__(self, index):
-        return self._hloldings[index]
+        return self._holdings[index]
     
     def  __contains__(self, name):
-        return any([h.name == name for h in self._hloldings])
+        return any([h.name == name for h in self._holdings])
     
         
     @property
     def total_cost(self) -> float:
-        return sum([h.cost for h in self._hloldings])
+        return sum([h.cost for h in self._holdings])
         
         
-    def  tabulate_shares(self) -> dict:
+    def tabulate_shares(self) -> dict:
         from collections import Counter
         total_shares = Counter()
-        for h in self._hloldings:
+        for h in self._holdings:
             total_shares[h.name] += h.shares
         return total_shares
+    
             
 
 
-
 if __name__ == '__main__':
-    pass
+    with open('Data/portfolio.csv') as lines: 
+        port = Portfolio.from_csv(lines)
 
 
 
