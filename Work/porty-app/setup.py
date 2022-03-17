a68899824500ebde0fab2d30bdfeb39dc158a050
karl <karlskibba@t-online.de>
2022-03-17 20:32:56 +0100

    Chapter 9 finished.

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,17 @@
+'''
+Created on 17.03.2022
+
+@author: ho_ksk
+__updated__='2022-03-17 18:16:31'
+'''
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Karl Skibba",
+    author_email="karlskibba@t-online.de",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
